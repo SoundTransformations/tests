587ac165447da3bdb1abe38ef123dba225ce167c
AlbertGubau <albert.gubau01@estudiant.upf.edu>
2022-05-04 16:02:14 +0200

    Modified main.py to run on Windows

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+import sys
 
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 
-
-from Functions.models import utilFunctions as UF
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Functions/models/'))
+import utilFunctions as UF
 from Functions.transformations_interface import stftMorph_function as sT
 
 #Functions
